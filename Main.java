75240819af78f144d18e05791732c0ba45c6dd56
Albertcute <95848647+Albertcute@users.noreply.github.com>
2022-09-16 16:08:02 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.java2_inherit;
 
-/**
- *
- * @author Student.Admin
- */
 public class Main {
 
 	public static void main(String[] args) {
@@ -31,8 +24,6 @@ class Friends {
 		System.out.println("You are beautiful!");
 	}
         
-        //ma inherit ni sya nga variable sa class nga hobby brad gamit ang extends nga keyword
-        //pwede sad ka maka inherit diri ug mga functions or uban pa nga properties
         String hobby = "Travelling";
 }
 
