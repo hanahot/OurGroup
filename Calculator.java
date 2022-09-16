a75b13e48434080f550b6f2efa85f9d37b32e048
Albertcute <95848647+Albertcute@users.noreply.github.com>
2022-09-16 21:37:01 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,22 @@ package simple.calculator;
 import java.util.Scanner;
 public class Addition extends Computation{
     public Addition() {
+        
+    }
+    
+    void result(){
+        Scanner sum = new Scanner(System.in);
+        
+        System.out.print("First Num: ");
+        int x = sum.nextInt();
+        
+        System.out.print("Second Num: ");
+        int y = sum.nextInt();
+        
+        int add = x + y;
+        System.out.println("The total is " + add);
+    }
+}
 
 package simple.calculator;
 
