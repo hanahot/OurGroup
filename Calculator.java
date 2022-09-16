78adb9db0a45be24fbba02055af6aed8fde1f21d
Albertcute <21103850@usc.edu.ph>
2022-09-16 20:38:32 +0800

    additionedit

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,9 @@
-
 package simple.calculator;
 
 import java.util.Scanner;
 public class Addition extends Computation{
     public Addition() {
-        // <editor-fold defaultstate="collapsed" desc="Compiled Code">
-        /* 0: aload_0
-         * 1: invokespecial simplecalculator/Calculation."<init>":()V
-         * 4: return
-         *  */
-        // </editor-fold>
+        
     }
     
     void result(){
